6c0b5b7d589ebed13ab340019afd3cefb3b9c389
David Leiva <david.leiva@getlife.es>
2021-11-11 14:46:11 +0100

    Initial commit

---
+++ wp-content/themes/hello-elementor/header.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * This is the template that displays all of the <head> section, opens the <body> tag and adds the site's header.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<?php $viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' ); ?>
+	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+	
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+  	<script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+	
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<?php
+hello_elementor_body_open();
+
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
+	if ( did_action( 'elementor/loaded' ) && hello_header_footer_experiment_active() ) {
+		get_template_part( 'template-parts/dynamic-header' );
+	} else {
+		get_template_part( 'template-parts/header' );
+	}
+}
