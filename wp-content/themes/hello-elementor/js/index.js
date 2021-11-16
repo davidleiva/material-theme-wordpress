9cad06de346913bc5c06926723114fca4e5befbd
David Leiva <david.leiva@getlife.es>
2021-11-16 16:36:04 +0100

    add js functionality

--- wp-content/themes/hello-elementor/js/index.js
+++ wp-content/themes/hello-elementor/js/index.js
@@ -4,7 +4,7 @@ let navEls;
 function toggleMegaMenu(e) {
     e.preventDefault();
     MegaMenu && MegaMenu.classList.contains('active')
-    ? MegaMenu.classList.removespurce('active')
+    ? MegaMenu.classList.remove('active')
     : MegaMenu.classList.add('active')
     ;
 }
@@ -18,6 +18,11 @@ document.onreadystatechange = function () {
     if(navEls && navEls.length > 0) {
         navEls.forEach(navItem => {
             navItem.firstElementChild. addEventListener('click', toggleMegaMenu);
+        });
+        document.addEventListener('click', function(event) {
+            if(event.target.parentElement.classList.contains('menu-item-has-children') === false) {
+                MegaMenu.classList.remove('active');
+            }
         });
     }
 }
